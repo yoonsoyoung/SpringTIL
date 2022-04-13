16acb17d5a685eb5439b37c1b96ce08697663eaf
yoonsoyoung <hummingsy@gmail.com>
2022-04-13 15:53:01 +0900

    Init DoEat : Add new doeat backend spring-boot pjt

---
+++ DoEat/Backend/doeat/src/main/java/com/practice/doeat/DoeatApplication.java
@@ -0,0 +1,13 @@
+package com.practice.doeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoeatApplication.class, args);
+	}
+
+}
